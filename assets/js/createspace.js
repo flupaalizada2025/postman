d3ad5bee5d5fb02909df00d61234a8edd3425dde
Aladdin <a.alizada2025@flup.agency>
2025-07-11 18:00:53 +0400

    uncomplete createworkspaces step2

--- assets/js/createspace.js
+++ assets/js/createspace.js
@@ -1,19 +1,51 @@
+const data = {
+  option: "",
+  inputName: "",
+  type: "",
+  access: "",
+};
+//
+localStorage.setItem("workSpacesData", JSON.stringify(data));
+
+// Next Button
 function nextButton() {
   // Steps
   let step1 = document.getElementById("step1");
   let step2 = document.getElementById("step2");
 
-  // Options
-
+  // Options classnames change event
   step1.classList.add("d-none");
   step2.classList.remove("d-none");
 }
 
-function dallas() {
-  console.log(this.value);
-}
-// let options = document.querySelectorAll("#option");
+// Option buttons
+let options = document.querySelectorAll("#option");
 
-// const data = [(option = ""), (name = ""), (type = ""), (access = "")];
+options.forEach((option) => {
+  option.addEventListener("click", () => {
+    options.forEach((opt) => opt.removeAttribute("id"));
+    option.setAttribute("id", "active");
 
-// localStorage.setItem("workSpacesData", JSON.stringify(data));
+    // Upate Selected option on my data
+    data.option = option.innerText;
+    localStorage.setItem("workSpacesData", JSON.stringify(data));
+  });
+});
+
+// Type Buttons
+
+document.addEventListener("DOMContentLoaded", () => {
+  const typeBtns = document.querySelectorAll("#typeBtn");
+
+  typeBtns.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      const h6element = event.currentTarget.querySelector("h6");
+
+      if (h6element) {
+        const h6Value = h6element.textContent;
+        data.type = h6Value;
+        localStorage.setItem("workSpacesData", JSON.stringify(data));
+      }
+    });
+  });
+});
