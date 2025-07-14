a079922326b469cbdf584c7a65e88d83040e6836
Aladdin <a.alizada2025@flup.agency>
2025-07-14 12:58:09 +0400

    completed Workspaces page

--- assets/js/createspace.js
+++ assets/js/createspace.js
@@ -1,21 +1,24 @@
 const data = {
-  option: "",
   inputName: "",
+  option: "",
   type: "",
   access: "",
 };
-//
+
 localStorage.setItem("workSpacesData", JSON.stringify(data));
 
-// Next Button
-function nextButton() {
-  // Steps
-  let step1 = document.getElementById("step1");
-  let step2 = document.getElementById("step2");
+// Next Button Function
+let step1 = document.getElementById("step1");
+let step2 = document.getElementById("step2");
 
+function nextButton() {
   // Options classnames change event
+  // if (data.option !== " ") {
+  //   alert("Please, fill the type");
+  // } else {
   step1.classList.add("d-none");
   step2.classList.remove("d-none");
+  // }
 }
 
 // Option buttons
@@ -49,3 +52,46 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+// WorkspaseName button function
+
+let nameInput = document.getElementById("name");
+let createButton = document.getElementById("createButton");
+function workspaceName() {
+  // control the input
+  if (nameInput.value !== " ") {
+    createButton.classList.remove("disabled");
+  } else {
+    createButton.classList.add("disabled");
+  }
+  data.inputName = nameInput.value;
+  localStorage.setItem("workSpacesData", JSON.stringify(data));
+}
+
+// acsesProperty buttons display
+
+let typeBtns = document.querySelectorAll("#typeBtn");
+let acsesPropertys = document.querySelectorAll("#acsesProperty");
+
+typeBtns[0].addEventListener("click", () => {
+  acsesPropertys[0].classList.remove("d-none");
+  acsesPropertys[1].classList.add("d-none");
+  acsesPropertys[2].classList.add("d-none");
+});
+
+typeBtns[1].addEventListener("click", () => {
+  acsesPropertys[1].classList.remove("d-none");
+  acsesPropertys[0].classList.add("d-none");
+  acsesPropertys[2].classList.add("d-none");
+});
+
+typeBtns[2].addEventListener("click", () => {
+  acsesPropertys[2].classList.remove("d-none");
+  acsesPropertys[1].classList.add("d-none");
+  acsesPropertys[0].classList.add("d-none");
+});
+
+// AcsesBtn
+let internalAcses = document.querySelectorAll(".internalAcses");
+
+console.log(internalAcses);
